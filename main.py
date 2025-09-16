a5b173942e08abc2b661a1e88fcf2b807efc044f
Nicollasm08 <marques.nicollas62@aluno.ifce.edu.br>
2025-09-16 11:52:52 +0000

    mudança

--- main.py
+++ main.py
@@ -27,11 +27,41 @@ def get_receita(nome: str):
             return r
     raise HTTPException(status_code=404, detail="Receita não encontrada")
 
-@app.get("/receitas/{id}")
+@app.get("/receitas/id/{id}")
 def get_receita(id: int):
     for r in receitas:
         if r.id == id:
             return r
     raise HTTPException(status_code=404, detail="Receita não encontrada")
 
+@app.post("/receita")
+def create_receita(dados: Receita):
+    nova_receita = dados
 
+    receitas.append(nova_receita)
+
+    return nova_receita
+
+@app.put("/receitas/{id}")
+def update_receita(id: int, dados: Receita):
+    for i in range(len(receitas)):
+        if receitas[i].id == id:
+            receita_atualizada = Receita(
+                id=id,
+                nome=dados.nome,
+                ingredientes=dados.ingredientes,
+                modo_de_preparo=dados.modo_de_preparo,
+            )
+            receitas[i] = receita_atualizada
+            return receita_atualizada
+
+    return {"mensagem": "Receita não encontrada"}
+
+
+@app.delete("/receitas/{id}")
+def deletar_receita(id: int):
+    for i in range(len(receitas)):
+        if receitas[i].id == id:
+            receitas.pop(i)
+            return {"Mensagem": "Receita deletada"}
+    return {"Mensagem": " Receita não encontrada"}
